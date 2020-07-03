0e5d984f74c43de313fefcdaa1616c5c6cb084d0
Baris Tikir <brstkr@icloud.com>
2020-07-03 19:14:37 +0200

    Home

---
+++ SmartHome/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace SmartHome
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<DummyItem> Items { get; set; }
+
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<DummyItem>()
+            {
+                new DummyItem { Title = "Living Room", Description = "4 devices to manage", Temperature = "21°C" },
+                new DummyItem { Title = "Dinning Room", Description = "4 devices to manage", Temperature = "21°C" },
+                new DummyItem { Title = "Kitchen", Description = "4 devices to manage", Temperature = "21°C" },
+            };
+        }
+    }
+
+    public class DummyItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Temperature { get; set; }
+    }
+}
